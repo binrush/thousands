c8eda7cee587b244f8f02cd7d156bd5b6a245188
Rushan Shaymardanov <rushan.shaymardanov@autodesk.com>
2015-08-31 13:54:48 +0500

    Debug for southural.ru auth

--- thousands/auth.py
+++ thousands/auth.py
@@ -1,12 +1,14 @@
 import json, httplib2, urllib, logging
 
 from flask.ext.login import login_user, logout_user, current_user, UserMixin
-from flask import request, g, redirect, make_response, url_for
+from flask import request, g, redirect, make_response, url_for, abort
 from oauth2client.client import FlowExchangeError
 
 from thousands import app
 
 httplib2.debuglevel = 4
+
+AUTH_SRC_VK = 1
 
 @app.route('/login/su')
 def su_login():
@@ -19,59 +21,36 @@ def su_login():
         logging.exception(e)
         return make_response('Error getting access token')
     
-    print credentials.token_response
     conn = httplib2.Http()
     credentials.authorize(conn)
     resp, content = conn.request('http://www.southural.ru/oauth2/UserInfo', 'POST')
-    print resp
-    print content
-    return make_response('')
+    return make_response(content)
 
 @app.route('/login/vk')
 def vk_login():
+    return oauth_login(request, g.vk_flow, vk_get_user)
+
+def oauth_login(req, flow, get_user):
     if 'error' in request.args.keys():
         return make_response(request.args.get('error_description'))
     
     try:
-        credentials = g.vk_flow.step2_exchange(request.args.get('code'))
+        credentials = flow.step2_exchange(request.args.get('code'))
     except FlowExchangeError, e:
         logging.exception(e)
         return make_response('Error getting access token')
 
-    # TODO remove magic number
-    user = g.users_dao.get(credentials.token_response['user_id'], 1)
-    if user is None:
-        profile = vk_get_profile(credentials)
-        user = UserMixin()
-        user.oauth_id = credentials.token_response['user_id']
-        user.name = profile['name']
-        user.src = 1
-        user.email = credentials.token_response.get('email', None)
-        
-        fd = urllib.urlopen(profile['image'])
-        if fd.getcode() == 200:
-            img_id = g.images_dao.create(fd.read(), fd.info().gettype())
-        else:
-            img_id = None
+    user = get_user(credentials)
+    if user is not None:
+        login_user(user)
+        return redirect(url_for('profile'))
 
-        user.img_id = img_id
-        user.id = g.users_dao.create(user)
-
-    login_user(user)
-    return redirect(url_for('profile'))
-
-def oauth_login(req, flow):
-     if 'error' in request.args.keys():
-        return make_response(request.args.get('error_description'))
-    
-    try:
-        credentials = g.vk_flow.step2_exchange(request.args.get('code'))
-    except FlowExchangeError, e:
-        logging.exception(e)
-        return make_response('Error getting access token')
+    abort(401)
    
-
-def vk_get_profile(credentials):
+def vk_get_user(credentials):
+    user = g.users_dao.get(credentials.token_response['user_id'], AUTH_SRC_VK)
+    if user is not None:
+        return user
 
     conn = httplib2.Http()
     credentials.authorize(conn)
@@ -80,20 +59,35 @@ def vk_get_profile(credentials):
             'fields'      : 'photo_100'
             }
     resp, content = conn.request('https://api.vk.com/method/users.get', 'POST', urllib.urlencode(params))
+
     if resp.status != 200:
         logging.error("Error getting user profile from vk api: %d", resp.status)
         logging.error(content)
         return None
+
     data = json.loads(content)['response'][0]
     if 'error' in data:
+        logging.error("Error getting profile data")
         logging.error(data['error'])
         return None
-    profile = {}
-    profile['name'] = u"{} {}".format(data['first_name'], data['last_name'])
-    profile['image'] = data['photo_100']
-    return profile
+    
+    user = UserMixin()
+    user.oauth_id = credentials.token_response['user_id']
+    user.name = u"{} {}".format(data['first_name'], data['last_name'])
+    user.src = AUTH_SRC_VK
+    user.email = credentials.token_response.get('email', None)
+        
+    fd = urllib.urlopen(data['photo_100'])
+    if fd.getcode() == 200:
+        user.img_id = g.images_dao.create(fd.read(), fd.info().gettype())
+    else:
+        user.img_id = None
 
-def su_get_profile(credentials):
+    user.id = g.users_dao.create(user)
+
+    return user
+
+def su_get_user(credentials):
 
     conn = httplib2.Http()
     credentials.authorize(conn)
